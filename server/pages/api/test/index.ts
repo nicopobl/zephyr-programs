0ef190570184186896435c3aa4f1fd94494645da
Nicolás Poblete <nipobletem@gmail.com>
2024-12-10 17:55:12 -0300

    Swagger test

--- server/pages/api/test/index.ts
+++ server/pages/api/test/index.ts
@@ -1,5 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+/**
+ * @swagger 
+ * /api/test
+ *   get:
+ *     description: API for test purposes
+ *     responses: 
+ *       200:
+ *         description: Hello World!
+ *       400:
+ *         description: Not Found
+ */
 async function handler(req: NextApiRequest, res: NextApiResponse) {
     return res.json("Hello world!");
 }
